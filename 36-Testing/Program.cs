37e72bfd54602bd2354ec76e5b61dc6903375a8b
David Steedman <dave@voxace.com>
2023-11-02 10:46:28 +1100

    Update Program.cs

--- 36-Testing/Program.cs
+++ 36-Testing/Program.cs
@@ -14,8 +14,21 @@ namespace _36_Testing
         {
             // Write your code here
 
+            Console.Write("How old are you: ");
+            int age = Convert.ToInt32(Console.ReadLine());
 
-
+            if(age >= 75)
+            {
+                Console.WriteLine("Ineligible");                
+            }
+            else if(age >= 18)
+            {
+                Console.WriteLine("Eligible to vote!");
+            }
+            else
+            {
+                Console.WriteLine("Ineligible");
+            }
 
             // Wait for input before ending
             Console.ReadLine();
