1a66bbd8af1af13eababcda9a9e6ae617278ce29
David Steedman <dave@voxace.com>
2023-11-16 10:41:48 +1100

    Update Program.cs

--- 36-Testing/Program.cs
+++ 36-Testing/Program.cs
@@ -14,24 +14,45 @@ namespace _36_Testing
         {
             // Write your code here
 
-            Console.Write("How old are you: ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            // I want a program that gets the user's name, asks for two numbers, then multiplies the two numbers
+            // Let's write some functions! What functions do we need?
+            // First: What are functions and how are they different to methods?           
 
-            if(age >= 75)
-            {
-                Console.WriteLine("Ineligible");                
-            }
-            else if(age >= 18)
-            {
-                Console.WriteLine("Eligible to vote!");
-            }
-            else
-            {
-                Console.WriteLine("Ineligible");
-            }
+            string name = GetName();
+            SayHello(name);
+
+            Console.Write("Please Enter a number: ");
+            int x = GetNumber();
+
+            Console.Write("Please Enter a number: ");
+            int y = GetNumber();
+
+            Console.WriteLine($"Your answer is {MultiplyNumbers(x, y)}");
 
             // Wait for input before ending
             Console.ReadLine();
+        }
+
+        static int MultiplyNumbers(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static int GetNumber()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+
+        static string GetName()
+        {
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
